1941b13b778fa404d96e2b0d61f6173cbc851a1f
Renato Cortez <renato.cortez220@gmail.com>
2021-08-15 13:58:09 -0500

    first commit

---
+++ Models/contacto.cs
@@ -0,0 +1,10 @@
+namespace PROGRA.Models
+{
+    public class contacto
+    {
+        public string correo { get; set;}
+        public string nombre { get;set;}
+        public string telefono { get;set;}
+        public string comentarios { get;set;}
+    }
+}
